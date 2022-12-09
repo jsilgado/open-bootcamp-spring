3175ba0a612d00e6083fcf29dcee13157d9fe42f
Jesús Silgado Carrero <jesussilgadocarrero@gmail.com>
2022-12-09 12:18:52 +0100

    ejercicio 1

---
+++ open-bootcamp-spring-ej1/src/main/java/jsilgado/spring/openbootcampspringej1/Saludo.java
@@ -0,0 +1,11 @@
+package jsilgado.spring.openbootcampspringej1;
+
+public class Saludo {
+
+    public Saludo(){
+        System.out.println("Ejecutando constructor Saludo");
+    }
+    public String imprimirSaludo(){
+        return "Hola que tal?";
+    }
+}
